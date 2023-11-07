c33bb57b867e181ca7da3dfc6bbf05d65072e605
yaduraj1301 <yaduraj1301km@gmail.com>
2023-11-07 20:11:47 +0530

    Landing page progress

---
+++ resources/Front-end/landing/script.js
@@ -0,0 +1,14 @@
+const nextBtn = document.querySelector('#next');
+const backBtn = document.querySelector('#back');
+
+nextBtn.addEventListener("click", function(event) {
+  const blog = document.querySelector('.collections');
+  blog.scrollLeft += 300;
+  event.preventDefault();
+});
+
+backBtn.addEventListener("click", function(event) {
+  const blog = document.querySelector('.collections');
+  blog.scrollLeft -= 300;
+  event.preventDefault();
+});
