b9b16cbc74e66ab480cca15b9632c6306f1c14c1
yaduraj1301 <yaduraj1301km@gmail.com>
2023-11-08 22:36:59 +0530

    Categories added

--- resources/Front-end/landing/script.js
+++ resources/Front-end/landing/script.js
@@ -3,12 +3,12 @@ const backBtn = document.querySelector('#back');
 
 nextBtn.addEventListener("click", function(event) {
   const blog = document.querySelector('.blogs');
-  blog.scrollLeft += 300;
+  blog.scrollLeft += 100;
   event.preventDefault();
 });
 
 backBtn.addEventListener("click", function(event) {
   const blog = document.querySelector('.blogs');
-  blog.scrollLeft -= 300;
+  blog.scrollLeft -= 100;
   event.preventDefault();
 });
