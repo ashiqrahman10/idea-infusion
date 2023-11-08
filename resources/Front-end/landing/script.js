4585300e712d5f32bfdefcfe4e7df6b261059bbe
yaduraj1301 <yaduraj1301km@gmail.com>
2023-11-08 13:30:44 +0530

    Landing modified

--- resources/Front-end/landing/script.js
+++ resources/Front-end/landing/script.js
@@ -2,13 +2,13 @@ const nextBtn = document.querySelector('#next');
 const backBtn = document.querySelector('#back');
 
 nextBtn.addEventListener("click", function(event) {
-  const blog = document.querySelector('.collections');
+  const blog = document.querySelector('.blogs');
   blog.scrollLeft += 300;
   event.preventDefault();
 });
 
 backBtn.addEventListener("click", function(event) {
-  const blog = document.querySelector('.collections');
+  const blog = document.querySelector('.blogs');
   blog.scrollLeft -= 300;
   event.preventDefault();
 });
