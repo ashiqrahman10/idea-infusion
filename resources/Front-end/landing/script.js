d0d8400324020d81a7389a7544aa99d0e9d4297a
yaduraj1301 <yaduraj1301km@gmail.com>
2023-11-09 17:26:28 +0530

    creators added on landing

--- resources/Front-end/landing/script.js
+++ resources/Front-end/landing/script.js
@@ -12,3 +12,18 @@ backBtn.addEventListener("click", function(event) {
   blog.scrollLeft -= 100;
   event.preventDefault();
 });
+
+const nextBtn2 = document.querySelector('#next2');
+const backBtn2= document.querySelector('#back2');
+
+nextBtn2.addEventListener("click", function(event) {
+  const cf = document.querySelector('.cframe');
+  cf.scrollLeft += 100;
+  event.preventDefault();
+});
+
+backBtn2.addEventListener("click", function(event) {
+  const cf = document.querySelector('.cframe');
+  cf.scrollLeft -= 100;
+  event.preventDefault();
+});
